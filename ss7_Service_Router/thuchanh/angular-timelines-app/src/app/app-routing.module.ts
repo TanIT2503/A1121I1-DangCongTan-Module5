a87e3e67ab44acbcdb8a9f56f994e1c880499ac0
Đặng Công Tấn <congtan2503@gmail.com>
2022-09-07 12:34:06 +0700

    update

---
+++ ss7_Service_Router/thuchanh/angular-timelines-app/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {TimelineComponent} from './timeline/timeline.component';
+
+const routes: Routes = [
+  {
+    path: 'timelines',
+    component: TimelineComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
