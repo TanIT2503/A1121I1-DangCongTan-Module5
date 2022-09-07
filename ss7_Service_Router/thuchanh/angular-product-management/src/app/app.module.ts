d23a70a4d9d0e0cacdd642ea7abf7822b490aa9a
Đặng Công Tấn <congtan2503@gmail.com>
2022-09-07 12:16:27 +0700

    Update bài tập

---
+++ ss7_Service_Router/thuchanh/angular-product-management/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './product/product-list/product-list.component';
+import { ProductCreateComponent } from './product/product-create/product-create.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {ReactiveFormsModule} from '@angular/forms';
+import { ProductEditComponent } from './product/product-edit/product-edit.component';
+
+
+// @ts-ignore
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductListComponent,
+    ProductCreateComponent,
+    ProductEditComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatFormFieldModule,
+    MatIconModule,
+    ReactiveFormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ ss7_Service_Router/thuchanh/angular-timelines-app/src/app/timeline/timeline.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import {DateUtilService} from '../service/date-util.service';
+
+@Component({
+  selector: 'app-timeline',
+  templateUrl: './timeline.component.html',
+  styleUrls: ['./timeline.component.css']
+})
+export class TimelineComponent implements OnInit {
+  output = '';
+
+  constructor(private dateUtilService: DateUtilService) {
+  }
+
+  ngOnInit() {
+  }
+
+  onChange(value) {
+    this.output = this.dateUtilService.getDiffToNow(value);
+  }
+
+
+}
